ccf128155c52114c15fc6f130305477cb9695212
Michal Doležel <michaldolezel123@gmail.com>
2023-06-03 16:35:49 +0200

    Ghost Lantern block decoratively valid

---
+++ src/main/java/com/terra/ghostz/GhostLanternBlock.java
@@ -0,0 +1,37 @@
+package com.terra.ghostz;
+
+import java.util.List;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LanternBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class GhostLanternBlock extends LanternBlock {
+    protected static final VoxelShape STANDING_SHAPE = VoxelShapes.cuboid(0.22f, 0f, 0.22f, 0.78f, 0.7f, 0.78f);
+    protected static final VoxelShape HANGING_SHAPE = VoxelShapes.cuboid(0.22f, 0.07f, 0.22f, 0.78f, 0.77f, 0.78f);
+
+    public GhostLanternBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return state.get(HANGING) != false ? HANGING_SHAPE : STANDING_SHAPE;
+    }
+
+    // @Override
+    // public void appendTooltip(ItemStack itemStack, BlockView world, List<Text>
+    // tooltip, TooltipContext tooltipContext) {
+    // tooltip.add(Text.translatable("block.ghostz.ghost_lantern.tt_charged"));
+    // tooltip.add(Text.translatable("block.ghostz.ghost_lantern.tt_bonuses"));
+
+    // }
+}
--- src/main/java/com/terra/ghostz/GhostZMod.java
+++ src/main/java/com/terra/ghostz/GhostZMod.java
@@ -2,10 +2,13 @@ package com.terra.ghostz;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -17,7 +20,11 @@ public class GhostZMod implements ModInitializer {
     public static final String MOD_ID = "ghostz";
     public static final Logger LOGGER = LoggerFactory.getLogger("ghostz");
 
-    public static final Block GHOST_LANTERN_BLOCK = new GhostLantern(FabricBlockSettings.copy(Blocks.SOUL_LANTERN));
+    public static final GhostLanternBlock GHOST_LANTERN = new GhostLanternBlock(FabricBlockSettings.copy(Blocks.SOUL_LANTERN));
+
+    private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier("ghostz", "item_group"))
+            .icon(() -> new ItemStack(GHOST_LANTERN))
+            .build();
 
     @Override
     public void onInitialize() {
@@ -26,7 +33,11 @@ public class GhostZMod implements ModInitializer {
         // Proceed with mild caution.
         LOGGER.info("GhostZ initialization");
 
-        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "ghost_lantern_block"), GHOST_LANTERN_BLOCK);
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "ghost_lantern_block"), new BlockItem(GHOST_LANTERN_BLOCK, new FabricItemSettings().maxCount(1)));
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "ghost_lantern"), GHOST_LANTERN);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "ghost_lantern"), new BlockItem(GHOST_LANTERN, new FabricItemSettings().maxCount(1)));
+
+        ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP).register(content -> {
+            content.add(GHOST_LANTERN);
+        });
     }
 }
