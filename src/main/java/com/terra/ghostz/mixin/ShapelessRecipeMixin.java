d8359d6c1f98ea34d78cc77f30cdd4c8d8c2ed8b
Michal Doležel <michaldolezel123@gmail.com>
2023-07-08 23:28:38 +0200

    change: made safe uuid getters and safer /clear mixin

--- src/main/java/com/terra/ghostz/mixin/ShapelessRecipeMixin.java
+++ src/main/java/com/terra/ghostz/mixin/ShapelessRecipeMixin.java
@@ -1,7 +1,5 @@
 package com.terra.ghostz.mixin;
 
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
